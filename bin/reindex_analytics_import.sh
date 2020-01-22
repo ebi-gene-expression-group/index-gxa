0ea55759939055f179e5193ab7fed314e136eb80
Suhaib Mohammed <suhaib@ebi.ac.uk>
2020-01-22 14:28:54 +0000

    adding reindex_analytics_import.sh script

---
+++ bin/reindex_analytics_import.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# This is a very I/O and memory intensive process. The application will first build a hash of all the gene annotations keyed by gene ID
+# and then will concurrently index all experiments, the biggest first in order to maximize parallelization. The first four to five hours will be “silent”
+# (i.e. there will be no updates in the progress page). You can alternatively have a look at the web application logs to make sure things are progessing smoothly.
+# The last optimization step effectively copies the full index into a consolidated set of files and replaces the old one. This also takes approximately three to four hours.
+
+scriptDir=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+[ -z ${ATLASPROD_PATH+x} ] && echo "Env var ATLASPROD_PATH for the path to atlas prod software check out needs to be defined." && exit 1
+[ ! -z ${TOMCAT_HOST_USERNAME+x} ] || ( echo "Env var TOMCAT_HOST_USERNAME ie. curator needs to be defined." && exit 1 )
+[ ! -z ${TOMCAT_HOST_PASSWORD+x} ] || ( echo "Env var TOMCAT_HOST_PASSWORD needs to be defined." && exit 1 )
+[ ! -z ${TOMCAT_HOST+x} ] || ( echo "Env var TOMCAT_HOST ie. ves:8080 needs to be defined." && exit 1 )
+[ ! -z ${EXP_ACC+x} ] || ( echo "Env var EXP_ACC ie. E-*-* needs to be defined." && exit 1 )
+
+# Set path (this is done at this level since this will be executed directly):
+for mod in gxa-data-migration/bin; do
+  export PATH=$ATLASPROD_PATH/$mod:$PATH
+done
+
+# start indexing analytics
+echo "Started analytics indexing ..."
+curl -s -u $TOMCAT_HOST_USERNAME:$TOMCAT_HOST_PASSWORD "http://$TOMCAT_HOST/gxa/admin/experiments/$EXP_ACC/ANALYTICS_IMPORT" 
+if [ $? -ne 0 ]; then
+    echo "ERROR: analytics indexing for $EXP_ACC on $TOMCAT_HOST"
+    exit 1
+fi
+
