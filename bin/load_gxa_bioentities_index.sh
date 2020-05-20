a325da643380f06f98af2737fcf78d3481e4e410
Suhaib Mohammed <suhaib@ebi.ac.uk>
2020-05-20 16:16:59 +0100

    adding tests files update

---
+++ bin/load_gxa_bioentities_index.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e
+
+[ -z ${BIOENTITIES_TSV+x} ] && echo "BIOENTITIES_TSV env var is needed." && exit 1
+
+export SCHEMA_VERSION=1
+export SOLR_COLLECTION=bioentities-v$SCHEMA_VERSION
+export PROCESSOR=$SOLR_COLLECTION\_dedup
+export ONTOLOGY_PROCESSOR=$SOLR_COLLECTION\_ontology_expansion
+
+echo "Loading bioentities $BIOENTITIES_TSV into host $SOLR_HOST collection $SOLR_COLLECTION..."
+
+cat $BIOENTITIES_TSV | jsonFilterEmptyFields.sh | loadJSONIndexToSolr.sh
