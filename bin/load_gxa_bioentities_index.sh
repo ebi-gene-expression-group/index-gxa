00fb0c91ebdbe5434152ad0baafe9a6036c85250
Suhaib Mohammed <suhaib@ebi.ac.uk>
2020-06-18 19:13:29 +0100

    removed all the references of solr-ontology-update-processor-1.1.jar and processors that is not used

--- bin/load_gxa_bioentities_index.sh
+++ bin/load_gxa_bioentities_index.sh
@@ -6,8 +6,6 @@ set -e
 
 export SCHEMA_VERSION=1
 export SOLR_COLLECTION=bioentities-v$SCHEMA_VERSION
-export PROCESSOR=$SOLR_COLLECTION\_dedup
-export ONTOLOGY_PROCESSOR=$SOLR_COLLECTION\_ontology_expansion
 
 echo "Loading bioentities $BIOENTITIES_TSV into host $SOLR_HOST collection $SOLR_COLLECTION..."
 
