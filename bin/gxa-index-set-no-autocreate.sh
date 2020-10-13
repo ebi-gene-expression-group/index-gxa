83c280b6f25bef329efe238145e77e0442f8b5ba
Suhaib Mohammed <suhaib@ebi.ac.uk>
2020-10-13 11:02:03 +0100

    hardcoded version number and removed temp files

--- bin/gxa-index-set-no-autocreate.sh
+++ bin/gxa-index-set-no-autocreate.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-SCHEMA_VERSION=${SCHEMA_VERSION:-1}
+SCHEMA_VERSION=1
 
 # on developers environment export SOLR_HOST_PORT and export SOLR_COLLECTION before running
 HOST=${SOLR_HOST:-"localhost:8983"}
