d68da8cb3c1c4e28958c7cd9d77bf771af53a4d5
Andrey Solovyev <a.solovyev39@gmail.com>
2020-11-20 18:05:24 +0000

    use exported variables instead of arguments

--- bin/gxa-index-check-experiments.sh
+++ bin/gxa-index-check-experiments.sh
@@ -1,20 +1,21 @@
 #!/usr/bin/env bash
 # check the health of experiments
 
-# $1: path to the list of experiments 
-# $2: minimum number of matches for experiment to pass the check 
-# $3: minimum number of matches to avoid warnings 
+[ ! -z ${SOLR_CLOUD_HOST+x} ] || ( echo "Env var SOLR_CLOUD_HOST needs to be defined." && exit 1 )
+[ ! -z ${ATLAS_EXPS+x} ] || (echo "Env var ATLAS_EXPS needs to be defined." && exit 1)
+[ ! -z ${EXP_MATCH_MIN+x} ] || (echo "Env var EXP_MATCH_MIN needs to be defined." && exit 1)
+[ ! -z ${EXP_MATCH_WARN+x} ] || (echo "Env var EXP_MATCH_WARN needs to be defined." && exit 1)
 
-experiments=$(ls $1)
+experiments=$(ls $ATLAS_EXPS)
 # extract number of entries per experiment 
-out=$(curl "http://wp-np2-87:8983/solr/bulk-analytics-v1/select?fl=experiment_accession&q=*:*&rows=10000000&group=true&group.field=experiment_accession")
+out=$(curl "http://$SOLR_CLOUD_HOST/solr/bulk-analytics-v1/select?fl=experiment_accession&q=*:*&rows=10000000&group=true&group.field=experiment_accession")
 for exp in $experiments; do
     n_entries=$(echo $out | grep -A 1 $exp | egrep -o '"numFound":[0-9]+' | egrep -o "[0-9]+")
-    if [ "$n_entries" -lt $2 ]; then
-        echo "Error: experiment $exp has $n_entries entries which is below the minimum of $2."
+    if [ "$n_entries" -lt $EXP_MATCH_MIN ]; then
+        echo "Error: experiment $exp has $n_entries entries which is below the minimum of $EXP_MATCH_MIN."
         expFail=true
-    elif [ "$n_entries" -lt $3 ]; then
-        echo "Warning: experiment $exp has $n_entries. Experiments are expected to have over $3 entries." 
+    elif [ "$n_entries" -lt $EXP_MATCH_WARN ]; then
+        echo "Warning: experiment $exp has $n_entries. Experiments are expected to have over $EXP_MATCH_WARN entries." 
     fi
 done
 if [ "$expFail" = true ]; then
