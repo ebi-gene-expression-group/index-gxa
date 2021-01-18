04699de1e92e9c5f583cd26fe3de9bb7a53f7f49
Andrey Solovyev <a.solovyev39@gmail.com>
2021-01-18 17:51:16 +0000

    update variable name to avoid conflicts (#6)
    * update variable name to avoid conflicts
    
    * update variable name to avoid conflicts
    
    * update tests

--- bin/gxa-index-check-experiments.sh
+++ bin/gxa-index-check-experiments.sh
@@ -2,7 +2,7 @@
 # check the health of experiments
 
 [ ! -z ${SOLR_HOST+x} ] || ( echo "Env var SOLR_HOST needs to be defined." && exit 1 )
-[ ! -z ${ATLAS_EXPS+x} ] || (echo "Env var ATLAS_EXPS needs to be defined." && exit 1)
+[ ! -z ${EXPERIMENT_ID+x} ] || (echo "Env var EXPERIMENT_ID needs to be defined." && exit 1)
 [ ! -z ${EXP_MATCH_MIN+x} ] || (echo "Env var EXP_MATCH_MIN needs to be defined." && exit 1)
 [ ! -z ${EXP_MATCH_WARNING+x} ] || (echo "Env var EXP_MATCH_WARN needs to be defined." && exit 1)
 [ ! -z ${EXPERIMENT_TYPE+x} ] || (echo "Env var EXPERIMENT_TYPE needs to be defined." && exit 1)
@@ -14,7 +14,7 @@ if [[ "$status" -ne "0" ]]; then
     echo $out
     exit 1
 fi 
-for exp in $ATLAS_EXPS; do
+for exp in $EXPERIMENT_ID; do
     n_entries=$(echo "$out" | grep -A 1 $exp | egrep -o '"numFound":[0-9]+' | egrep -o "[0-9]+")
     if [[ $n_entries -lt $EXP_MATCH_MIN ]]; then
         echo "Error: experiment $exp has $n_entries entries which is below the minimum of $EXP_MATCH_MIN."
