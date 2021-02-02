e75b9fbb45ca731c9095a553097761c7f7adda91
Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>
2021-02-02 22:26:42 +0000

    Test creation of suggesters and /suggest request handler

---
+++ tests/bioentities-check-created-suggesters-gxa.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+SCHEMA_VERSION=1
+
+set -e
+
+# on developers environment export SOLR_HOST_PORT and export SOLR_COLLECTION before running
+HOST=${SOLR_HOST:-"localhost:8983"}
+CORE=${SOLR_COLLECTION:-"bioentities-v$SCHEMA_VERSION"}
+
+echo "Checking suggesters..."
+
+HTTP_STATUS=$(curl -s -w "%{http_code}" -o /dev/null "http://$HOST/solr/$CORE/suggest?suggest.dictionary=propertySuggester")
+
+if [[ ! $HTTP_STATUS == 2* ]];
+then
+  # HTTP Status is not a 2xx code, so it is an error.
+  exit 1
+fi
