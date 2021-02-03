049da20eb93551cda99a298d311be8938733c9c8
Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>
2021-02-03 00:12:32 +0000

    Move test script to tests

--- tests/gxa-index-check-experiments.sh
+++ tests/gxa-index-check-experiments.sh
@@ -5,14 +5,13 @@
 [ -z ${EXPERIMENT_ID+x} ] && echo "Env var EXPERIMENT_ID needs to be defined." && exit 1
 [ -z ${EXP_MATCH_MIN+x} ] && echo "Env var EXP_MATCH_MIN needs to be defined." && exit 1
 [ -z ${EXP_MATCH_WARNING+x} ] && echo "Env var EXP_MATCH_WARN needs to be defined." && exit 1
-[ -z ${EXPERIMENT_TYPE+x} ] && echo "Env var EXPERIMENT_TYPE needs to be defined." && exit 1
 
-out=$(curl --silent "http://$SOLR_HOST/solr/$EXPERIMENT_TYPE-analytics-v1/select?fl=experiment_accession&q=*:*&rows=10000000&group=true&group.field=experiment_accession")
-status="$?" 
+out=$(curl --silent "http://$SOLR_HOST/solr/gxa-analytics-v1/select?fl=experiment_accession&q=*:*&rows=10000000&group=true&group.field=experiment_accession")
+status="$?"
 if [ "$status" -ne "0" ]; then
     echo $out
     exit 1
-fi 
+fi
 for exp in $EXPERIMENT_ID; do
     n_entries=$(echo "$out" | grep -A 1 -e "\"$exp\"" | egrep -o '"numFound":[0-9]+' | egrep -o "[0-9]+")
     [ -z "$n_entries" ] && n_entries=0
@@ -20,14 +19,12 @@ for exp in $EXPERIMENT_ID; do
         echo "Error: experiment $exp has $n_entries entries which is below the minimum of $EXP_MATCH_MIN."
         expFail=true
     elif [ "$n_entries" -lt "$EXP_MATCH_WARNING" ]; then
-        echo "Warning: experiment $exp has $n_entries matches. Experiments are expected to have over $EXP_MATCH_WARNING entries." 
+        echo "Warning: experiment $exp has $n_entries matches. Experiments are expected to have over $EXP_MATCH_WARNING entries."
     fi
 done
 if [ "$expFail" = true ]; then
     echo "Experiments with insufficient entries found - see out/errors"
     exit 1
 fi
-echo "All indexed experiments have sufficient entries - Successful Run" 
+echo "All indexed experiments have sufficient entries - Successful Run"
 exit 0
-
-
