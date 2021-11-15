b99cd2901aa38f11d390e7aa78a66ea950cb1cee
Pedro Madrigal <8195212+pmb59@users.noreply.github.com>
2021-11-15 13:56:35 +0000

    Update generate_analytics_JSONL_files.sh

--- bin/generate_analytics_JSONL_files.sh
+++ bin/generate_analytics_JSONL_files.sh
@@ -1,3 +1,46 @@
+#!/usr/bin/env bash
 
-java -jar ./build/libs/atlas-cli-bulk.jar \
-bulk-analytics-json -o ./bulk-analytics -i ./bioentities/mus-musculus.map.bin -e E-MTAB-3578,E-MTAB-3579,E-MTAB-3358
+jar_dir=$CONDA_PREFIX/share/atlas-cli
+
+require_env_var "SOLR_HOST"
+require_env_var "ZK_HOST"
+require_env_var "ZK_PORT"
+require_env_var "BIOENTITIES"
+require_env_var "SPECIES" 
+require_env_var "ACCESSIONS"
+require_env_var "output_dir"
+
+SOLR_PORT=$(get_port_from_hostport $SOLR_HOST)
+SOLR_HOST=$(get_host_from_hostport $SOLR_HOST)
+
+require_env_var "SOLR_PORT"
+
+java_opts="-Dsolr.host=$SOLR_HOST"
+java_opts="$java_opts -Dsolr.port=$SOLR_PORT"
+java_opts="$java_opts -Dzk.host=$ZK_HOST"
+java_opts="$java_opts -Dzk.port=$ZK_PORT"
+java_opts="$java_opts -Ddata.files.location=$BIOENTITIES"
+java_opts="$java_opts -Dexperiment.files.location=$EXPERIMENT_FILES"
+java_opts="$java_opts -Djdbc.url=$jdbc_url"
+java_opts="$java_opts -Djdbc.username=$jdbc_username"
+java_opts="$java_opts -Djdbc.password=$jdbc_password"
+java_opts="$java_opts -Dserver.port=$server_port"
+
+# Generate JSONL files from bulk experiments
+
+cmd="java $java_opts -jar $jar_dir/atlas-cli-bulk.jar"
+cmd=$cmd" bulk-analytics-json -o $output_dir -i $BIOENTITIES/$SPECIES.map.bin " 
+
+
+status=0
+if [ -z ${ACCESSIONS+x} ]; then
+  # we have no accessions
+  echo "Env variable ACCESSIONS not defined"
+  status=1
+else
+  # we run for specific accessions
+  $cmd -e $ACCESSIONS
+  status=$?
+fi
+
+exit $status
