733e315e5acace522ebefc7d75c56a82bfbac57b
Pedro Madrigal <8195212+pmb59@users.noreply.github.com>
2021-11-15 14:47:55 +0000

    add wrapper for Load analytics files in Solr

--- bin/load_analytics_files_in_Solr.sh
+++ bin/load_analytics_files_in_Solr.sh
@@ -1,7 +1,23 @@
+#!/usr/bin/env bash
+
+require_env_var "SOLR_HOST"
+require_env_var "ZK_HOST"
+require_env_var "ZK_PORT"
+require_env_var "ACCESSIONS"
+require_env_var "output_dir"
+
 # Load analytics files in Solr
 
+failed=0
 
-for EXP_ID in E-MTAB-3578 E-MTAB-3579 E-MTAB-3358
+for FILE in $output_dir/$ACCESSIONS
 do
-  INPUT_JSONL=./bulk-analytics/${EXP_ID}.jsonl SOLR_COLLECTION=bulk-analytics SCHEMA_VERSION=1 SOLR_PROCESSORS=dedupe ./bin/solr-jsonl-chunk-loader.sh
+  INPUT_JSONL=$FILE SOLR_COLLECTION=bioentities SOLR_COLLECTION=bulk-analytics SCHEMA_VERSION=1 SOLR_PROCESSORS=dedupe ./bin/solr-jsonl-chunk-loader.sh
+  if [ $? -ne 0 ]; then
+    echo "Loading JSONL to $SOLR_HOST failed for $FILE"
+    failed=1
+  fi
 done
+
+exit $failed
+
