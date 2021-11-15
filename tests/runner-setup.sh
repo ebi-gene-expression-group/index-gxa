d152bbca9e1e6ee70cc28217c1916b0c99de3dff
Pedro Madrigal <8195212+pmb59@users.noreply.github.com>
2021-11-15 17:27:22 +0000

    Update runner-setup.sh

--- tests/runner-setup.sh
+++ tests/runner-setup.sh
@@ -3,13 +3,3 @@
 DIR=$(dirname ${BASH_SOURCE[0]})
 export PATH=$DIR/../bin:$DIR/../tests:$PATH
 
-# For java cli
- export ZK_HOST=${ZK_HOST:localhost}
- export ZK_PORT=${ZK_PORT:-2181}
- export BIOENTITIES=$DIR/fixtures/
- export EXPERIMENT_FILES=$DIR/fixtures/experiment_files
- export jdbc_url=$jdbc_url
- export SPECIES=homo_sapiens
- export jdbc_username=$POSTGRES_USER
- export jdbc_password=$POSTGRES_PASSWORD
- export server_port=8081 #fake
