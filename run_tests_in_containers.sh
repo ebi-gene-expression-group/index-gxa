025611c95565e5ff954eb7c3f42bbccb12ee7bc5
Pedro Madrigal <8195212+pmb59@users.noreply.github.com>
2021-11-18 20:48:41 +0000

    add -t to docker exec

--- run_tests_in_containers.sh
+++ run_tests_in_containers.sh
@@ -62,7 +62,7 @@ docker run --rm -i --net mynet \
 # disable data driven schema functionality - not recommended for production
 # curl http://localhost:8983/solr/bulk-analytics-v1/config -d '{"set-user-property": {"update.autoCreateFields":"false"}}'
 
-docker exec -i --user=solr my_solr bin/solr create_collection -c bulk-analytics-v1
+docker exec -it --user=solr my_solr bin/solr create_collection -c bulk-analytics-v1
 
 
 docker run --rm -i --net mynet -v $( pwd )/tests:/usr/local/tests \
