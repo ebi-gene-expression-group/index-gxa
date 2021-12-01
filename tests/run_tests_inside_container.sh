60812acc704e36ffb332da10c36232836af8429d
Pablo Moreno <pablo.a.moreno@gmail.com>
2021-12-01 11:22:52 +0000

    whoami

--- tests/run_tests_inside_container.sh
+++ tests/run_tests_inside_container.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+whoami
 ls -l /usr/local/tests/fixtures/experiment_files/expdesign
 chmod a+w /usr/local/tests/fixtures/experiment_files/expdesign
 ls -l /usr/local/tests/fixtures/experiment_files/expdesign
