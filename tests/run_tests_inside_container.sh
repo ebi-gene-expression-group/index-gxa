a5c17b6f30571cd8563ce4a0eaf492eee5329a3d
Pablo Moreno <pablo.a.moreno@gmail.com>
2021-12-01 11:38:54 +0000

    Clean up

--- tests/run_tests_inside_container.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-whoami
-ls -l /usr/local/tests/fixtures/experiment_files/expdesign
-chmod a+w /usr/local/tests/fixtures/experiment_files/expdesign
-ls -l /usr/local/tests/fixtures/experiment_files/expdesign
-
-/usr/local/tests/run-tests.sh
