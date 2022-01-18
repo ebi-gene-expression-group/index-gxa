bd4120331bb05587cd5a918590ba40f90251df17
Pablo Moreno <pablo.a.moreno@gmail.com>
2022-01-18 17:34:55 +0000

    try minimumIdle instead of minimum-idle

--- bin/update_experiment_designs_cli.sh
+++ bin/update_experiment_designs_cli.sh
@@ -32,7 +32,7 @@ java_opts="$java_opts -Djdbc.url=$jdbc_url"
 java_opts="$java_opts -Djdbc.username=$jdbc_username"
 java_opts="$java_opts -Djdbc.password=$jdbc_password"
 java_opts="$java_opts -Dserver.port=$server_port"
-java_opts="$java_opts -Dspring.datasource.hikari.minimum-idle=2"
+java_opts="$java_opts -Dspring.datasource.hikari.minimumIdle=2"
 
 # Generate JSONL files from bulk experiments
 
