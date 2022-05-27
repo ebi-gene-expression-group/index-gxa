f19b50838258f7574d3af2419d00181860dbbe51
Pablo Moreno <pablo.a.moreno@gmail.com>
2022-05-27 22:39:27 +0100

    Correct deletion script bulk analytics

--- bin/load_analytics_files_in_Solr.sh
+++ bin/load_analytics_files_in_Solr.sh
@@ -23,7 +23,7 @@ fi
 for EXP_ID in $ACCESSIONS; do
   export EXP_ID # needed for delete to see it
   if [ ! -z ${delete_existing+x} ]; then
-    $scriptDir/delete_gxa_analytics_index.sh
+    $scriptDir/delete_bulk_analytics_index.sh
   fi
   INPUT_JSONL=${analytics_jsonl_dir}/${EXP_ID}.jsonl SOLR_COLLECTION=bulk-analytics SCHEMA_VERSION=1 solr-jsonl-chunk-loader.sh  # SOLR_COLLECTION=bulk-analytics SOLR_PROCESSORS=dedupe
   if [ $? -ne 0 ]; then
