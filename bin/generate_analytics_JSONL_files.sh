2f174eb44613ab66ee8c895937dbca799da60745
Amnon Khen <amnon@ebi.ac.uk>
2026-06-02 16:10:37 +0100

    Update Java options in analytics scripts to include '/solr' in Solr host URLs gxa-599

--- bin/generate_analytics_JSONL_files.sh
+++ bin/generate_analytics_JSONL_files.sh
@@ -25,7 +25,7 @@ SOLR_HOST=$(get_host_from_hostport $SOLR_HOST)
 
 require_env_var "SOLR_PORT"
 
-java_opts="-Dsolr.hosts=http://$SOLR_HOST:$SOLR_PORT"
+java_opts="-Dsolr.hosts=http://$SOLR_HOST:$SOLR_PORT/solr"
 java_opts="$java_opts -Dsolr.port=$SOLR_PORT"
 java_opts="$java_opts -Dzk.hosts=http://$ZK_HOST:$ZK_PORT"
 java_opts="$java_opts -Dzk.port=$ZK_PORT"
